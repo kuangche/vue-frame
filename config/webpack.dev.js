4a3354038e27f3c24903f6d8800d711343c134c6
jige <935505012@qq.com>
2019-03-01 18:35:03 +0800

    fix(prox): add prox

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,7 +7,14 @@ module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
-        hot: true
+        hot: true,
+        proxy: {
+            '/api': {
+                target: 'http://www.mininglamp.com', // 表示你跨域请求的接口的域名
+                changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+                pathRewrite: { '^/api': '' } // 后面可以使重写的新路径，一般不做更改
+            }
+        }
     },
     plugins: [
         new webpack.NamedModulesPlugin(),
